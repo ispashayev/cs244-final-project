875804a467160ab7d6ae90a0191e19b728dca1db
Iskandar Pashayev <ispashayev@gmail.com>
2018-05-26 11:20:41 -0700

    fixed typo

--- scripts/mount-efs.sh
+++ scripts/mount-efs.sh
@@ -14,4 +14,4 @@ sudo apt-get install nfs-common
 sudo mkdir efs
 
 # Mount the EFS
-sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-(echo $EFS_ID).efs.us-west-1.amazonaws:/ efs
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-(echo $EFS_ID).efs.us-west-1.amazonaws.com:/ efs
