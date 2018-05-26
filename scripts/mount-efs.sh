b28cab1e790449eca467164b2a95ff5cf7d5c5e3
Iskandar Pashayev <ispashayev@gmail.com>
2018-05-26 11:18:26 -0700

    Added EFS mount script for AWS instances

---
+++ scripts/mount-efs.sh
@@ -0,0 +1,17 @@
+# Mounts the EFS containing the test data sets to an instance. Note: your
+# instance needs to be added to our VPC security group for this script to work
+# since we need to hard-code the EFS id's. Alternatively, you can replace the
+# id's with your own private EFS id.
+
+# TODO: remove hard-coded information as much as possible.
+
+EFS_ID="e302e4fa"
+
+# Install NFS client
+sudo apt-get install nfs-common
+
+# Create directory for EFS
+sudo mkdir efs
+
+# Mount the EFS
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-(echo $EFS_ID).efs.us-west-1.amazonaws:/ efs
