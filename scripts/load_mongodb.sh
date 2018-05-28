f03dcb86e7d65f9b2d87dcd89c5586b311ecbd51
Ubuntu <ubuntu@ip-172-31-15-54.us-west-1.compute.internal>
2018-05-28 07:42:54 +0000

    fixed tar

--- scripts/load_mongodb.sh
+++ scripts/load_mongodb.sh
@@ -2,17 +2,24 @@
 
 # Adds hourly weather data from weatherstations around the globe into mongod
 
+cd ~/efs;
+
 START=2015
 END=2018
 
 for YR in $(seq $START $END); do
+    if [ $YR -eq 2017 ]; then
+	# Already loaded data for 2017
+	continue
+    fi
+
     # Download global hourly climate data for the year
-    sudo wget https://www.ncei.noaa.gov/data/global-hourly/archive/$YR.tar.gz -P=~/efs/;
+    sudo wget https://www.ncei.noaa.gov/data/global-hourly/archive/$YR.tar.gz;
 
     # Extract contents
-    sudo tar -xzvf ~/efs/$YR.tar.gz;
+    sudo tar -xzvf $YR.tar.gz;
     
-    for weatherstationdata in ~/efs/*.csv; do
+    for weatherstationdata in ./*.csv; do
 	mongoimport -d noaaClimateData -c globalHourly --type csv --file "$weatherstationdata" --headerline && sudo rm -f "$weatherstationdata";
     done
 done
